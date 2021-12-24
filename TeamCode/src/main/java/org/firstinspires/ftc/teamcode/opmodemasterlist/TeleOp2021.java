a19a2ae1540c5a9ff207286fd7f3b80ed18f5f41
yashpermalla <67378117+yashpermalla@users.noreply.github.com>
2021-12-24 17:34:40 -0500

    Update TeleOp2021.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodemasterlist/TeleOp2021.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodemasterlist/TeleOp2021.java
@@ -82,9 +82,9 @@ public class TeleOp2021 extends LinearOpMode {
 
 
             //annoying trig drivetrain stuff
-            double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
-            double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
-            double rightX = gamepad1.right_stick_x;
+            double r = Math.hypot(-1 * gamepad1.left_stick_x, -1 * gamepad1.left_stick_y);
+            double robotAngle = Math.atan2(-1 * gamepad1.left_stick_y, -1 * gamepad1.left_stick_x) - Math.PI / 4;
+            double rightX = -1 * gamepad1.right_stick_x;
             final double v1 = r * Math.cos(robotAngle) + rightX;
             final double v2 = r * Math.sin(robotAngle) - rightX;
             final double v3 = r * Math.sin(robotAngle) + rightX;
